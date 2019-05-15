14be6015d19a1bcf5a81ea512cabdc5bd1d4faea
JapaBits <34574051+wellingtonj1@users.noreply.github.com>
2019-05-15 10:46:06 -0300

    Update main.cpp

--- bfsprogram/main.cpp
+++ bfsprogram/main.cpp
@@ -1,12 +1,32 @@
 #include <iostream>
 #include <bits/stdc++.h>
-
+#include <vector>
+#include "pessoas.h"
 
 using namespace std;
 
 int main()
 {
+    string nome;
+    int i;
+    vector<pessoas> pessoas;
+    vector<int> vertices;
 
+    int quantpes;
+    cin>>quantpes;
+    cin.ignore();
+    pessoas.resize(quantpes);
+
+    for(i=0;i<quantpes;i++)
+    {
+        getline(cin,nome);
+        pessoas[i].setnome(nome);
+    }
+
+    for(i=0;i<quantpes;i++)
+    {
+        cout<<pessoas[i].getnome()<<endl;
+    }
 
 
 }
