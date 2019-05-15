1f2b396af8451d63003158fdc3e78587cb21b91c
JapaBits <34574051+wellingtonj1@users.noreply.github.com>
2019-05-15 11:38:29 -0300

    Update main.cpp

--- bfsprogram/main.cpp
+++ bfsprogram/main.cpp
@@ -5,27 +5,62 @@
 
 using namespace std;
 
+void getprint(vector<pessoas> pes, int quant,vector<int>a,vector<int>d)
+{
+    int y;
+    cout<<"   ";
+    for(y=0;y<quant;y++)
+    {
+        cout<<pes[y].getnome()<<"   ";
+    }
+    puts("");
+    for(y=0;y<quant;y++)
+    {
+        cout<<pes[y].getnome()<<endl;
+    }
+}
 int main()
 {
     string nome;
-    int i;
+    int i,quantiligas,j,k;
     vector<pessoas> pessoas;
-    vector<int> vertices;
+    vector<int> linhas,colunas;
 
     int quantpes;
+    puts("Insira quantidade de pessoas");
     cin>>quantpes;
     cin.ignore();
     pessoas.resize(quantpes);
-
+    linhas.resize(quantpes);
+    colunas.resize(quantpes);
     for(i=0;i<quantpes;i++)
     {
         getline(cin,nome);
         pessoas[i].setnome(nome);
     }
+    puts("Quantidade de ligações?");
+    cin>>quantiligas;
 
+    puts("Fale as ligações ..");
+    vector<int>x,y;
+    x.resize(quantiligas);
+    y.resize(quantiligas);
+    for(i=0;i<quantiligas;i++)
+    {
+        cin>>j>>k;
+        x.push_back(j);
+        y.push_back(k);
+    }
+    getprint(pessoas,quantpes,x,y);
     for(i=0;i<quantpes;i++)
     {
-        cout<<pessoas[i].getnome()<<endl;
+        for(int w=0;w<quantpes;w++)
+        {
+            if(i==x[w]&&y[i]==w)
+            {
+                cout<<1<<endl;
+            }
+        }
     }
 
 
